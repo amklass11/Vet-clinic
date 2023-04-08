2b74f7dca53bd7b645c18d08b4d8cec9b55d12ee
amklass11 <lassanaa14@gmail.com>
2023-04-08 08:42:07 +0100

    alter table

--- data.sql
+++ data.sql
@@ -1,7 +1,14 @@
-/* Populate database with sample data. */
+/* Database schema to keep the structure of entire database. */
 
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
-('Agumon','2020-02-03','0','True','10.23'),
-('Gabumon','2018-11-15','2','True','8.00'),
-('Pikachu','2021-01-07','1','False','15.04'),
-('Devimon','2017-05-12','5','True','11.00');
+CREATE TABLE animals(
+ id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(100) NOT NULL,
+date_of_birth DATE NULL,
+escape_attempts INT NOT NULL,
+neutered BOOLEAN NOT NULL,
+weight_kg DECIMAL NOT NULL,
+PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+ADD species TEXT;
