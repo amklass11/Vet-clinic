c4dfd79c5863eb43c7cd79a53e024c4976cdd4f7
amklass11 <lassanaa14@gmail.com>
2023-04-08 08:55:20 +0100

    Table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+/* Database schema to keep the structure of entire database. */
+
 CREATE TABLE animals(
  id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR(100) NOT NULL,
@@ -7,3 +9,6 @@ neutered BOOLEAN NOT NULL,
 weight_kg DECIMAL NOT NULL,
 PRIMARY KEY(id)
 );
+
+ALTER TABLE animals
+ADD species TEXT;
